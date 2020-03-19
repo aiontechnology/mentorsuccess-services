339da8b402ce67186bf8a4b55f0794001f388fd3
Whitney Hunter <whitney@aiontechnology.io>
2020-03-19 12:08:23 -0700

    Initial checkin

---
+++ src/main/java/io/aiontechnology/mentorsuccess/MentorsuccessApplication.java
@@ -0,0 +1,13 @@
+package io.aiontechnology.mentorsuccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorsuccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorsuccessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/aiontechnology/mentorsuccess/MentorsuccessApplicationTests.java
@@ -0,0 +1,13 @@
+package io.aiontechnology.mentorsuccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorsuccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
