0c8a6b2610b6b62357168d89b5b1837b6659b275
Whitney Hunter <whitney@aiontechnology.io>
2021-01-08 14:22:42 -0800

    MS-152 Added media release flag and location to mentors

--- model-lib/src/main/java/io/aiontechnology/mentorsuccess/model/outbound/student/OutboundStudentMentor.java
+++ model-lib/src/main/java/io/aiontechnology/mentorsuccess/model/outbound/student/OutboundStudentMentor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Aion Technology LLC
+ * Copyright 2020-2021 Aion Technology LLC
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@
 
 package io.aiontechnology.mentorsuccess.model.outbound.student;
 
+import io.aiontechnology.mentorsuccess.model.enumeration.ResourceLocation;
 import io.aiontechnology.mentorsuccess.model.outbound.OutboundMentor;
 import lombok.Builder;
 import lombok.Value;
@@ -31,5 +32,9 @@ public class OutboundStudentMentor {
     OutboundMentor mentor;
 
     String time;
+
+    ResourceLocation location;
+
+    Boolean mediaReleaseSigned;
 
 }
