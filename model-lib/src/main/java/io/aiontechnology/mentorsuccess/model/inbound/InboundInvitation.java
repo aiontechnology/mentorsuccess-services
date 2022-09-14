0ec104e9290df067c8fcfd39cec09d1eb2734b84
Whitney Hunter <whitney@aiontechnology.io>
2022-09-14 15:08:11 -0700

    MS-36 Implemented dialog plus email for student invitation

---
+++ model-lib/src/main/java/io/aiontechnology/mentorsuccess/model/inbound/InboundInvitation.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2022 Aion Technology LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.aiontechnology.mentorsuccess.model.inbound;
+
+import lombok.Builder;
+import lombok.Value;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Value
+@Builder(setterPrefix = "with")
+public class InboundInvitation {
+    @NotNull()
+    @Size(max = 100)
+    String parentFirstName;
+
+    @NotNull()
+    @Size(max = 100)
+    String parentLastName;
+
+    @NotNull()
+    @Size(max = 100)
+    String parentEmailAddress;
+
+    @NotNull()
+    @Size(max = 100)
+    String studentFirstName;
+
+    @NotNull()
+    @Size(max = 100)
+    String studentLastName;
+
+}
