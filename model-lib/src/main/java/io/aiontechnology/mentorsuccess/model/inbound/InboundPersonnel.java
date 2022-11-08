5ef7d7cfde520117ef7a1713258dc6e9024e8e15
Whitney Hunter <whitney@aiontechnology.io>
2022-11-08 10:12:48 -0800

    MS-538 Fixed phone number validations to allow empty values

--- model-lib/src/main/java/io/aiontechnology/mentorsuccess/model/inbound/InboundPersonnel.java
+++ model-lib/src/main/java/io/aiontechnology/mentorsuccess/model/inbound/InboundPersonnel.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2020 Aion Technology LLC
+ * Copyright 2020-2022 Aion Technology LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,6 +18,7 @@ package io.aiontechnology.mentorsuccess.model.inbound;
 
 import io.aiontechnology.mentorsuccess.model.enumeration.RoleType;
 import io.aiontechnology.mentorsuccess.model.validation.EnumNamePattern;
+import io.aiontechnology.mentorsuccess.model.validation.ValidOrNullPhone;
 import lombok.Builder;
 import lombok.Value;
 
@@ -55,11 +56,11 @@ public class InboundPersonnel {
     String email;
 
     /** The work phone number. */
-    @Pattern(regexp = "\\(\\d{3}\\) \\d{3}-\\d{4}", message = "{personnel.workPhone.invalid}")
+    @ValidOrNullPhone(message = "{personnel.workPhone.invalid}")
     String workPhone;
 
     /** The cell phone number. */
-    @Pattern(regexp = "\\(\\d{3}\\) \\d{3}-\\d{4}", message = "{personnel.cellPhone.invalid}")
+    @ValidOrNullPhone(message = "{personnel.cellPhone.invalid}")
     String cellPhone;
 
 }
--- model-lib/src/main/java/io/aiontechnology/mentorsuccess/model/inbound/InboundTeacher.java
+++ model-lib/src/main/java/io/aiontechnology/mentorsuccess/model/inbound/InboundTeacher.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2020 Aion Technology LLC
+ * Copyright 2020-2022 Aion Technology LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,6 +16,7 @@
 
 package io.aiontechnology.mentorsuccess.model.inbound;
 
+import io.aiontechnology.mentorsuccess.model.validation.ValidOrNullPhone;
 import lombok.Builder;
 import lombok.Value;
 
@@ -50,11 +51,11 @@ public class InboundTeacher {
     String email;
 
     /** The teacher's work phone number. */
-    @Pattern(regexp = "\\(\\d{3}\\) \\d{3}-\\d{4}", message = "{teacher.workPhone.invalid}")
+    @ValidOrNullPhone(message = "{teacher.workPhone.invalid}")
     String workPhone;
 
     /** The teacher's cell phone */
-    @Pattern(regexp = "\\(\\d{3}\\) \\d{3}-\\d{4}", message = "{teacher.cellPhone.invalid}")
+    @ValidOrNullPhone(message = "{teacher.cellPhone.invalid}")
     String cellPhone;
 
     /** First grade taught by teacher. */
