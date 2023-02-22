add5d739df9bf715438020970408421922f71977
Whitney Hunter <whitney@aiontechnology.io>
2023-02-22 12:35:38 -0800

    MS-831 Fixed interest and school sessions

--- server/src/main/java/io/aiontechnology/mentorsuccess/service/SchoolSessionService.java
+++ server/src/main/java/io/aiontechnology/mentorsuccess/service/SchoolSessionService.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2022 Aion Technology LLC
+ * Copyright 2022-2023 Aion Technology LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -64,6 +64,10 @@ public class SchoolSessionService {
                                     .map(StudentSchoolSession::new)
                                     .map(StudentSchoolSession::incrementGrade)
                                     .map(studentSchoolSession -> studentSchoolSession.setSession(newSchoolSession))
+                                    .map(studentSchoolSession -> {
+                                        studentSchoolSession.setIsRegistrationSigned(Boolean.TRUE);
+                                        return studentSchoolSession;
+                                    })
                                     .collect(Collectors.toList());
 
                     newSchoolSession.setStudentSchoolSessions(newStudentSchoolSessions);
